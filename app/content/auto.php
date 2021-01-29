e4cc19433028f4f9989601af142816b1905525c7
KlimovCode <klimovcode@mail.ru>
2021-01-29 16:03:23 +0500

    catalog cards done

--- app/content/auto.php
+++ app/content/auto.php
@@ -2,8 +2,9 @@
     <div class="container">
         <h2 class="main__title">Каталог</h2>
         <div class="main__inner">
-            <?php include "./db/getbrends.php"; ?>
-
+            <div class="cards__wrap">
+                <?php include "./db/getbrends.php"; ?>
+            </div>
         </div>
     </div>
 </main>
--- app/db/getbrends.php
+++ app/db/getbrends.php
@@ -12,10 +12,10 @@ $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($lin
 if($result) {
     while ($row = mysqli_fetch_row($result)) {
 //        "<a href=\"./auto/a$row[0]\">$row[1]</a>"
-        echo '<div class="common-card">';
-        echo "<img src=\"img/img/Audi/logo.jpg\" alt=\"\" class=\"common-card__img\">
-               <p class=\"common-card__title\">Audi</p>";
-        echo '</div>';
+        echo '<div class="common-card">' . "<a href=\"./auto/a$row[0]\"  class=\"common-card__link\">";
+        echo "<img src=\"img/$row[2]\" alt=\"\" class=\"common-card__img\">
+               <p class=\"common-card__title\">$row[1]</p>";
+        echo '</a></div>';
     }
     // очищаем результат
     mysqli_free_result($result);
