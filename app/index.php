e21056d48c3928fc1c9a901e14beca1047548e82
KlimovCode <klimovcode@mail.ru>
2021-01-29 21:08:37 +0500

    update routing for get models

--- app/index.php
+++ app/index.php
@@ -2,27 +2,34 @@
 
 include "./partials/top.php";
 include "./partials/header.php";
-
-switch ($_GET['route']) {
-    case 'app/auto':
-        include "./content/auto.php";
-        break;
-    case 'app/mounting':
-        include "./content/mounting.php";
-        break;
-    case 'app/about':
-        include "./content/about.php";
-        break;
-    case 'app/contacts':
-        include "./content/contacts.php";
-        break;
-    default:
-        include "./partials/intro.php";
-        include "./partials/overview.php";
-        include "./partials/benefits.php";
-        include "./partials/labels.php";
-        break;
+if(preg_match("/app\/auto\/a(\d{1,2}|50)$/", $_GET['route'])) {
+    $brend = explode('/', $_GET['route']);
+    $brend = $brend[2];
+    
+} else {
+    switch ($_GET['route']) {
+        case 'app/auto':
+            include "./content/auto.php";
+            break;
+        case 'app/mounting':
+            include "./content/mounting.php";
+            break;
+        case 'app/about':
+            include "./content/about.php";
+            break;
+        case 'app/contacts':
+            include "./content/contacts.php";
+            break;
+        default:
+            include "./partials/intro.php";
+            include "./partials/overview.php";
+            include "./partials/benefits.php";
+            include "./partials/labels.php";
+            break;
+    }
 }
+
+
 
 include "./partials/footer.php";
 include "./partials/bottom.php";
