af148329b3e4d47a97b2e910e44aa99a6e3aee24
KlimovCode <klimovcode@mail.ru>
2021-01-30 22:31:03 +0500

    Update getsubmodels.php

--- app/db/getsubmodels.php
+++ app/db/getsubmodels.php
@@ -7,15 +7,14 @@ or die("Ошибка " . mysqli_error($link));
 
 // get id of brend
 $model = explode('/', $_GET['route']);
-$model = $model[3];
-$model_id = (int)substr($model, 1);
+$model_id = (int)substr($model[3], 1);
 
 $query ="SELECT * FROM submodels WHERE id_model='$model_id'";
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 
 if($result) {
     while ($row = mysqli_fetch_row($result)) {
-        echo '<div class="common-card">' . "<a href=\"./a$brend_id/m$row[0]\"  class=\"common-card__link\">";
+        echo '<div class="common-card">' . "<a href=\"/app/auto/$model[2]/$model[3]/sm$row[0]\"  class=\"common-card__link\">";
         echo "<img src=\"/app/img/$row[2]\" alt=\"\" class=\"common-card__img\">
                <p class=\"common-card__title\">$row[1]</p>";
         echo '</a></div>';
