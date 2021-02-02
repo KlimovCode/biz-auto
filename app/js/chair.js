6074b8d726289c3e099ef571325025c338bd3341
KlimovCode <klimovcode@mail.ru>
2021-02-02 22:09:25 +0500

    add changable for big img

--- app/js/chair.js
+++ app/js/chair.js
@@ -11,4 +11,12 @@ $(document).ready(function() {
         $(".chair__color").css({display: 'none'})
         $(".chair__color--"+fabric).css({display: 'flex'})
     })
+
+    $(".chair__color__item").on('click', function (e) {
+        let temp = e.currentTarget.classList // [..]
+        let temp2 = temp[1].substr(-2) // [-9 - 99]
+        let img = temp2 < 0 ? temp[1].substr(-1) : temp2
+        $(".chair__bigimg").css({display: 'none'})
+        $(".chair__bigimg--"+img).show()
+    })
 })
