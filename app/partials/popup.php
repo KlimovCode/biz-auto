31181f21f37303be9b8ae4eb13e410879b1f29ab
KlimovCode <klimovcode@mail.ru>
2021-02-03 15:04:01 +0500

    fix popup btn

--- app/partials/popup.php
+++ app/partials/popup.php
@@ -3,7 +3,7 @@
         <h2>Оставьте свой номер и мы вам перезвоним</h2>
         <form action="/app/callback.php">
             <input type="text" placeholder="Ваш телефон" class="popup__callback__phone">
-            <div class="chair__btn popup__callback__btn">
+            <div class="popup__callback__btn">
                 <button class="popup__callback__buy" type="submit">
                     Заказать звонок
                 </button>
