33e460420d1b27f241642be5fe5629227df77a5c
KlimovCode <klimovcode@mail.ru>
2021-02-06 11:25:26 +0500

    update benefits and fix intro slider

--- app/js/rslider.js
+++ app/js/rslider.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
     var slideNow = 1;
     var slideCount = $('.rslides').children().length;
-    var slideInterval = 2000;
+    var slideInterval = 5000;
     var translateWidth = 0;
 
     function nextSlide() {
         if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-            $('.rslides').css('transform', 'translate(0, 0)');
+            $('.rslides').css({
+                'transform': 'translate(0, 0)',
+                'transition' : 'all 3s linear'
+            });
             slideNow = 1;
         } else {
             translateWidth = -$('.rslides li').width() * (slideNow);
             $('.rslides').css({
                 'transform': 'translate(' + translateWidth + 'px, 0)',
-                'transition' : 'all 1000ms linear',
+                'transition' : 'all 3000ms linear',
                 '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
                 '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
             });
