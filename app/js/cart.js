4e38d9536e52895eafde57745af2e832906cda41
KlimovCode <klimovcode@mail.ru>
2021-02-09 09:42:34 +0500

    cart data summ change done

--- app/js/cart.js
+++ app/js/cart.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
                                 </tr>
                             </table>
                         </td>
-                        <td>${goods[i].actualPrice}</td>
+                        <td id="price-${goods[i].time}">${goods[i].actualPrice}</td>
                     </tr>`
             }
         } else {
@@ -99,57 +99,16 @@ $(document).ready(function() {
 
     $(".cart__table__inside__input").click(function (e) {
         toggleInput(e)
-        // checkCheckedInput()
         actualPrice()
-        // showCart()
-        // test()
-        // checkCheckedInput()
-        // setTimeout(showCart, 100)
+        changePrice()
     })
 
-    // function test() {
-    //     let result = ""
-    //     if(localStorage.getItem('cart')) {
-    //         let cart = JSON.parse(localStorage.getItem('cart'))
-    //         let goods = cart.goods
-    //         result = "<tr><th>№</th><th>Модель</th><th>Цена</th></tr>"
-    //         for (let i = 0; i < goods.length; i++) {
-    //             result += `<tr>
-    //                     <td>${i+1}</td>
-    //                     <td>
-    //                         <table class="cart__table__inside">
-    //                             <tr>
-    //                                 <td colspan="2" class="cart__table__inside__name">${goods[i].titleAuto}</td>
-    //                             </tr>
-    //                             <tr>
-    //                                 <td>
-    //                                     ткань: ${goods[i].fabricRus}
-    //                                 </td>
-    //                                 <td>
-    //                                     цвет: ${goods[i].fabricColor}
-    //                                 </td>
-    //                             </tr>
-    //                             <tr>
-    //                                 <td>
-    //                                     <label for="setchechol-${goods[i].time}" class="cart__table__inside__input">
-    //                                         установка 1500 руб.
-    //                                         <input type="checkbox"
-    //                                             name="setchechol"
-    //                                             id="setchechol-${goods[i].time}">
-    //                                     </label>
-    //                                 </td>
-    //                                 <td>
-    //                                     <label for="sewchechol-${goods[i].time}" class="cart__table__inside__input">
-    //                                         подшивка 1000 руб.
-    //                                         <input type="checkbox" name="sewchechol" id="sewchechol-${goods[i].time}">
-    //                                     </label>
-    //                                 </td>
-    //                             </tr>
-    //                         </table>
-    //                       </td>`
-    //         }
-    //     }
-    //     $(".cart__table").html(result)
-    // }
+    function changePrice() {
+        let cart = JSON.parse(localStorage.getItem('cart'))
+        let goods = cart.goods
+        for (const good of goods) {
+            $("#price-"+good.time).text(good.actualPrice)
+        }
+    }
 
 })
