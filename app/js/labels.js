b445cfc1852ab643171465306454a0bfb5c5b49e
KlimovCode <klimovcode@mail.ru>
2021-02-10 00:22:03 +0500

    labels slider done

--- app/js/labels.js
+++ app/js/labels.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     var labelNow = 1;
     var labelCount = $('.labels__inner').children().length;
-    var labelInterval = 2000;
+    var labelInterval = 4000;
     var labelTranslateWidth = 0;
 
     function nextLabel() {
         if (labelNow == labelCount || labelNow <= 0 || labelNow > (labelCount - 6)) {
             $('.labels__inner').css({
                 'transform': 'translate(0, 0)',
-                'transition' : 'all 0.5s linear'
+                'transition' : 'all 1s linear'
             });
             labelNow = 1;
         } else {
             labelTranslateWidth = -$('.labels__inner .label__item').width() * (labelNow);
             $('.labels__inner').css({
                 'transform': 'translate(' + labelTranslateWidth + 'px, 0)',
-                'transition' : 'all 0.5s linear',
+                'transition' : 'all 1s linear',
                 '-webkit-transform': 'translate(' + labelTranslateWidth + 'px, 0)',
                 '-ms-transform': 'translate(' + labelTranslateWidth + 'px, 0)',
             });
@@ -23,4 +23,13 @@ $(document).ready(function() {
         }
     }
     setInterval(nextLabel, labelInterval);
+
+    $(".labels__arrow__left button").click(function () {
+        labelNow -= 2
+        nextLabel()
+        setTimeout(()=>{}, 1000)
+    })
+    $(".labels__arrow__right button").click(function () {
+        nextLabel()
+    })
 })
