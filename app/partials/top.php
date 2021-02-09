9bf96d3e431fb2877033b90fb66371ee3630af28
KlimovCode <klimovcode@mail.ru>
2021-02-09 10:05:27 +0500

    fix title

--- app/partials/top.php
+++ app/partials/top.php
@@ -6,7 +6,7 @@
     <link rel="icon" href="/app/img/dest/icon-32.jpg" sizes="32x32">
     <link rel="icon" href="/app/img/dest/icon-192.jpg" sizes="192x192">
     <link rel="apple-touch-icon" href="/app/img/dest/icon-180.jpg">
-    <title>ПрофАвто - авточехлы, сидения, коврики и автоаксессуары</title>
+    <title>ПрофАвто - авточехлы, накидки, коврики и автоаксессуары</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
