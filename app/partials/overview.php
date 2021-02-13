7f5afe31a7333d6cc3b227a4a985e4c7e786e3d4
KlimovCode <klimovcode@mail.ru>
2021-02-13 13:58:42 +0500

    overview as links

--- app/partials/overview.php
+++ app/partials/overview.php
@@ -1,7 +1,7 @@
 <div class="overview">
     <div class="container">
         <div class="overview__inner">
-            <div class="overview__card overview__card--first">
+            <a href="/app/auto" class="overview__card__link"><div class="overview__card overview__card--first">
                 <div class="overview__title">
                     <div class="overview__subtitle">Модельные</div>
                     Авточехлы
@@ -11,10 +11,10 @@
                     6500 руб.
                 </div>
                 <div class="overview__btn">
-                    <a href="/app/auto" class="overview__btn--link">В каталог</a>
+                    <span href="/app/auto" class="overview__btn--link">В каталог</span>
                 </div>
-            </div>
-            <div class="overview__card overview__card--second">
+            </div></a>
+            <a href="/app/auto" class="overview__card__link"><div class="overview__card overview__card--second">
                 <div class="overview__title">
                     <div class="overview__subtitle">Машинные</div>
                     Коврики
@@ -24,10 +24,10 @@
                     2500 руб.
                 </div>
                 <div class="overview__btn">
-                    <a href="/app/auto" class="overview__btn--link">В каталог</a>
+                    <span class="overview__btn--link">В каталог</span>
                 </div>
-            </div>
-            <div class="overview__card overview__card--third">
+            </div></a>
+            <a href="/app/auto" class="overview__card__link"><div class="overview__card overview__card--third">
                 <div class="overview__title">
                     Накидки
                     <div class="overview__subtitle">Для сидений</div>
@@ -37,9 +37,9 @@
                     700 руб.
                 </div>
                 <div class="overview__btn">
-                    <a href="/app/auto" class="overview__btn--link">В каталог</a>
+                    <span class="overview__btn--link">В каталог</span>
                 </div>
-            </div>
+            </div></a>
         </div>
     </div>
 </div>
