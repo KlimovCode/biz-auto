f1e8c22335f4bc317d42a505538c225e1bbfaaf0
KlimovCode <klimovcode@mail.ru>
2021-02-24 20:15:45 +0500

    update version of css file

--- app/partials/top.php
+++ app/partials/top.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/app/css/app.min.css">
+    <link rel="stylesheet" href="/app/css/app.min.css?v=1">
 
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
