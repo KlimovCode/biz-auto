0bae0dd870ab5760c1bd25f5d472e0109c412a32
KlimovCode <klimovcode@mail.ru>
2021-03-07 15:05:23 +0500

    work on rugs

--- app/content/rugs/models.php
+++ app/content/rugs/models.php
@@ -1,9 +1,9 @@
 <main class="main">
     <div class="container">
-        <h2 class="main__title">Марки</h2>
+        <h2 class="main__title">Модельный ряд</h2>
         <div class="main__inner">
             <div class="cards__wrap">
-                <?php require_once "./app/db/getbrendsrugs.php"; ?>
+                <?php require_once "./app/db/getmodels.php"; ?>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -10,6 +10,8 @@ if(preg_match("/app\/auto\/a\d{1,2}\/m\d{1,3}\/sm\d{1,2}/", $_GET['route'])) {
     require_once "./app/content/submodels.php";
 } elseif(preg_match("/app\/auto\/a(\d{1,2}|50)$/", $_GET['route'])) {
     require_once "./app/content/models.php";
+} elseif(preg_match("/app\/auto\/rugs(\d{1,2}|50)$/", $_GET['route'])) {
+    require_once "./app/content/rugs/rugs.php";
 } elseif(preg_match("/app\/auto\/rugs/", $_GET['route'])) {
     require_once "./app/content/rugs/rugs.php";
 } elseif(preg_match("/app\/cart/", $_GET['route'])) {
