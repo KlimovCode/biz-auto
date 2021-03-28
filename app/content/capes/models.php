f19c59ebc35e9cc890af28f8818fc45fbc8c8034
KlimovCode <klimovcode@mail.ru>
2021-03-28 22:20:07 +0500

    get title from database for cape brend

--- app/content/capes/models.php
+++ app/content/capes/models.php
@@ -1,6 +1,6 @@
 <main class="main">
     <div class="container">
-        <h2 class="main__title">test</h2>
+        <h2 class="main__title"><?php require_once "./app/db/getcapebrendtitle.php"; ?></h2>
         <div class="main__inner">
             <div class="cards__wrap">
 <!--                --><?php //require_once "./app/db/getmodelsrugs.php"; ?>
---
+++ app/db/getcapebrendtitle.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'connection.php'; // get variables
+
+$link = mysqli_connect($host, $username, $password, $dbname)
+or die("Ошибка " . mysqli_error($link));
+$link->set_charset("utf8");
+
+// get id of brend
+$brend = explode('/', $_GET['route']);
+preg_match('/(\d)$/', $brend[2], $brend_id);
+$brend_id = $brend_id[0];
+
+$query ="SELECT title FROM capes WHERE id='$brend_id'";
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+
+if($result) {
+    while ($row = mysqli_fetch_row($result)) {
+        echo $row[0];
+    }
+    // очищаем результат
+    mysqli_free_result($result);
+}
+
+mysqli_close($link);
