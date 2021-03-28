376c5e246ea73e5709ae9f0dbc7d1fa5b429a8ad
KlimovCode <klimovcode@mail.ru>
2021-03-28 19:49:29 +0500

    add routing for capes

---
+++ app/content/capes/capes.php
@@ -0,0 +1,10 @@
+<main class="main">
+    <div class="container">
+        <h2 class="main__title">Виды накидок</h2>
+        <div class="main__inner">
+            <div class="cards__wrap">
+<!--                --><?php //require_once "./app/db/getbrendsrugs.php"; ?>
+            </div>
+        </div>
+    </div>
+</main>
