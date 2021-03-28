f30c66470d8ba6e9127228567c0a6956bfd74060
KlimovCode <klimovcode@mail.ru>
2021-03-28 22:46:51 +0500

    add routing for cape item

---
+++ app/content/capes/item.php
@@ -0,0 +1,19 @@
+<main class="main">
+    <div class="container">
+        <h2 class="main__title">test<?php // require_once "./app/db/getchairnamerugs.php"?></h2>
+        <div class="main__inner">
+            <div class="chair__wrap">
+                <div class="chair__inner">
+                    <?php
+                    // require_once "./app/db/getimgsrugs.php";
+                    ?>
+                </div>
+                <div class="rug__btn">
+                    <button class="rug__btn__buy">
+                        Добавить в корзину
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
