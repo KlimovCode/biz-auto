41c83743095231e103bbe2f0a1c004e7bd7dbe87
KlimovCode <klimovcode@mail.ru>
2021-03-30 18:09:08 +0500

    get title of cape item

--- app/content/capes/item.php
+++ app/content/capes/item.php
@@ -1,6 +1,6 @@
 <main class="main">
     <div class="container">
-        <h2 class="main__title">test<?php // require_once "./app/db/getchairnamerugs.php"?></h2>
+        <h2 class="main__title"><?php require_once "./app/db/getchairnamecape.php"?></h2>
         <div class="main__inner">
             <div class="chair__wrap">
                 <div class="chair__inner">
